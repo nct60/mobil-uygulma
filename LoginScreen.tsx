43be0407513d0c28659568db4bc903ecc7add57d
nct60 <babacannecati@hotmail.com>
2024-12-21 18:48:06 +0300

    Create LoginScreen.tsx

---
+++ LoginScreen.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet, Text, Alert } from 'react-native';
+import CustomButton from '../components/CustomButton'; // Doğru yolu kontrol edin
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    Alert.alert('Login Attempt', `Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <CustomButton title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+});
+
+export default LoginScreen;
